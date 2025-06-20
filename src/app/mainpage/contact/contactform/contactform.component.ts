10bb3c401585642c353aa1de44fec998f12f4686
Irene Kober <irene.kober@gmx.net>
2025-06-20 11:07:37 +0200

    added form validation and styling to contact form

--- src/app/mainpage/contact/contactform/contactform.component.ts
+++ src/app/mainpage/contact/contactform/contactform.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contactform',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './contactform.component.html',
   styleUrl: './contactform.component.scss'
 })
 export class ContactformComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+      isChecked: [false, Validators.requiredTrue]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form data:', this.contactForm.value);
+    } else {
+      console.warn('Form is invalid');
+    }
+  }
 }
