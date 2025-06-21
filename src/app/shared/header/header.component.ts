cced40c867d8b6e106be3f0718b8aeb212496803
Irene Kober <irene.kober@gmx.net>
2025-06-21 14:19:26 +0200

    set up language center and button background color feedback

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { LanguageService } from '../../shared/services/language.service';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  currentLang: 'EN' | 'DE' = 'EN';
+
+  constructor(private languageService: LanguageService) {
+    this.languageService.currentLang.subscribe((lang) => {
+      this.currentLang = lang;
+    });
+  }
+
+  switchLang(lang: 'EN' | 'DE') {
+  this.languageService.setLang(lang);
+}
 
 }
