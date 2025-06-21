9d5800ef2e756ed671c01230ad63c95b77bba86c
Irene Kober <irene.kober@gmx.net>
2025-06-21 14:26:24 +0200

    translation available for Header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,21 +3,42 @@ import { LanguageService } from '../../shared/services/language.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  navText = {
+    about: '',
+    skills: '',
+    portfolio: '',
+  };
+
   currentLang: 'EN' | 'DE' = 'EN';
 
   constructor(private languageService: LanguageService) {
     this.languageService.currentLang.subscribe((lang) => {
       this.currentLang = lang;
+
+      switch (lang) {
+        case 'DE':
+          this.navText = {
+            about: 'Über mich',
+            skills: 'Kenntnisse',
+            portfolio: 'Portfolio',
+          };
+          break;
+        case 'EN':
+        default:
+          this.navText = {
+            about: 'About me',
+            skills: 'Skills',
+            portfolio: 'Portfolio',
+          };
+      }
     });
   }
 
   switchLang(lang: 'EN' | 'DE') {
-  this.languageService.setLang(lang);
-}
-
+    this.languageService.setLang(lang);
+  }
 }
