17e6d6137ba686c895c6756eeb56260384c7713d
Irene Kober <irene.kober@gmx.net>
2025-06-26 14:45:10 +0200

    added animation for error messages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 export const routes: Routes = [
     {path: '', component: MainpageComponent },
     {path: 'imprint', component: ImprintComponent},
+    {path: 'privacy', component: PrivacyComponent}
 ];
